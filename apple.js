b633f21a710e9fed98ecfc1c6e8f9c1a64d61cac
Zakaria878 <hzakaria3311@gmail.com>
2022-08-02 14:28:28 +0600

    js practice

---
+++ apple.js
@@ -0,0 +1,19 @@
+// Problem solving 01
+// var moneyGiven = 1000;
+// var applePrice = 400;
+// var orangePrice = 300;
+
+// var totalPrice = applePrice + orangePrice;
+// var getBack = moneyGiven - totalPrice;
+// console.log(getBack);
+
+// Problem solving 02
+var pMarks = 68;
+var cMarks = 57;
+var mMarks = 33.04;
+var bMarks = 43;
+var eMarks = 97;
+var totalMarks = pMarks + cMarks + mMarks + bMarks + eMarks;
+var avrageMarks = totalMarks / 5;
+var avgTowDecimal = avrageMarks.toFixed(4);
+console.log(avgTowDecimal);
---
+++ index.js
@@ -0,0 +1,59 @@
+// var someNumbers = [54, 87, 65, 89, 26];
+
+// 1. get element value by index
+//var element = someNumbers[1];
+//console.log(element);
+
+// 2. set element value by index
+//someNumbers[1] = 33;
+//someNumbers[4] = 55;
+//console.log(someNumbers);
+
+// 3. find index of an element
+//var positionIndex = someNumbers.indexOf(65);
+
+//someNumbers.push(20);
+//someNumbers.push("zakaria");
+///someNumbers.pop();
+//console.log(someNumbers);
+// var element = someNumbers.pop();
+// console.log(element);
+
+//  For, break
+// for(i=0; i<=20; i++){
+//     console.log(i);
+//     if(i>10){
+//         break;
+//     }
+// }
+
+// Continue
+// var numbers = [15, 49,84, 89, 45, 12, 35, 78, 75, 16, 57, 64, 13]
+// for (var i = 0; i<numbers.length; i++){
+//     var number = numbers[i];
+//     if(number>50){
+//         continue;
+//     }
+//     console.log(number)
+// }
+
+// while show 0-10
+// var num = 0;
+// while(num < 10){
+//     num++;
+//     console.log(num);
+// }
+
+// while lop reverse 10-1
+// var num = 10;
+// while(num >= 1 ){
+//     console.log(num);
+//     num--;
+// }
+
+for(var i = 0; i <= 10; i++){
+    console.log(i);
+}
+for(var i=10; i >= 1; i--){
+    console.log(i);
+}
