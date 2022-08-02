10250e280b7d1488939831e606a540cabce32752
Zakaria878 <hzakaria3311@gmail.com>
2022-08-02 16:49:30 +0600

    Some problem solving

---
+++ array-prb-solve.js
@@ -0,0 +1,6 @@
+var fruits = ["apple", "banana", "orange"];
+var indexFind = fruits.indexOf("banana");
+fruits[1]="kola";
+fruits.pop();
+fruits.push("watermelon");
+console.log(fruits);
---
+++ grade.js
@@ -0,0 +1,24 @@
+var marks = 39;
+if(marks>=80)
+{
+    console.log("A grade");
+}
+else if(marks>=60) 
+{
+    console.log("B grade");
+}
+else if (marks>=50) 
+{
+    console.log("C grade");
+}
+else if (marks>=40) 
+{
+    console.log("D grade");
+}
+else if (marks >= 39)
+{
+    console.log("F grade");
+}
+else{
+    console.log("Faild")
+}
