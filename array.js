059e78d516623b596d1fcef146f31dc4c5af3a50
Zakaria878 <hzakaria3311@gmail.com>
2022-08-07 22:33:56 +0600

    Slice

---
+++ array.js
@@ -0,0 +1,13 @@
+const country = 'Bangladesh';
+const age = 52;
+const isIndependent = true;
+const student = { id: 121, class: 11, name: 'Agun' };
+const friends = [13, 14, 11, 17, 21, 16, 15, 20]
+
+console.log(typeof country);
+//for array check
+console.log(Array.isArray(friends));
+console.log(friends.includes(19));
+if(friends.indexOf(19) !== -1){
+
+}
---
+++ foo.js
@@ -0,0 +1,4 @@
+const number = -78;
+const ans = Math.abs(number);
+
+console.log(ans);
